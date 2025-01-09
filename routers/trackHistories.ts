9b74b71aab399081a24dc0bc49f663c8639479f1
Beksultan Chomoev <chomoevbeksultan@gmail.com>
2025-01-08 22:21:01 -0600

    Authorization is done for track_history route.

--- routers/trackHistories.ts
+++ routers/trackHistories.ts
@@ -1,5 +1,24 @@
 import express from "express";
+import User from "../models/User";
 
 const trackHistoriesRouter = express.Router();
+
+trackHistoriesRouter.post('/', async (req, res) => {
+    const token = req.get('Authorization');
+
+    if (!token) {
+        res.status(401).send({error: 'No token presented'});
+        return;
+    }
+
+    const user = await User.findOne({token});
+
+    if (!user) {
+        res.status(401).send({error: 'Wrong token'});
+        return;
+    }
+
+    res.send({username: user.username, id_user: user._id});
+});
 
 export default trackHistoriesRouter;
